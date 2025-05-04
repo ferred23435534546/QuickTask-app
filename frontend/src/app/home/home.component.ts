461e5116e9a2c0a56cb420ef34682fcc333c8055
Andres0909 <andresjosecaparrosnicolas0909@gmail.com>
2025-05-04 21:25:49 +0200

    Nueva pagina principal

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common'; // Al principio del archivo
 
 @Component({
   selector: 'app-home',
+  standalone: true, // Asumiendo que es standalone
+  imports: [ CommonModule ], // <--- Aquí
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
   fechaActual = new Date(); 
+  isSearchVisible: boolean = false;
+  toggleSearch(): void {
+    this.isSearchVisible = !this.isSearchVisible;
+  }
 }
